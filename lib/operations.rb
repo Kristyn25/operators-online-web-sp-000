310304be81aad6a83e1448f5aadb01fe1d0f48f6
Kristyn Little <kristynlittle@frontier.com>
2021-02-05 06:06:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
 	if speed = 40 < 60
-		"unsafe"
+		true
 	else
-		"safe"
+		false
 	end
 end
 
 def not_safe?(speed)
-	speed = 40 < 60 ? "not_safe" : "safe"
+	speed = 40 < 60 ? true : false
 end
